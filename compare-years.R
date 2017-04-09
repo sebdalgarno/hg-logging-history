3de8a98701a3615c00ef37cc7590f6c0a8c338eb
sebdalgarno <seb.dalgarno@gmail.com>
2017-04-09 15:15:11 -0700

    initial commit

--- compare-years.R
+++ compare-years.R
@@ -12,16 +12,24 @@ inter %<>% mutate(VriAreaU = st_area(.))
 
 # calculate proportional area - filter if overlaps by less than 10%
 inter %<>% mutate(ProportionalArea = as.numeric(VriAreaU/AreaU))
-inter %<>% filter(ProportionalArea > 0.25 & as.numeric(VriAreaU) > 250)
+inter %<>% filter(ProportionalArea > 0.25)
 
 # year difference
 inter %<>% filter(!is.na(YearHarv) & YearHarv > 0 & Year > 0)
+inter %<>% filter(YearHarv < 2005)
 inter %<>% mutate(YearDiff = YearHarv - Year)
 
 # mean by log04 ID
 inter.grp <- group_by(inter, Log04ID) %>% summarise(AveYearDif = mean(YearDiff)) %>% st_cast("MULTIPOLYGON")
 
+# inter plot
 write_shp(inter.grp, layer = "log04-vri-intersection-summary", path = path.expand("~/Dropbox/Contracts/Gowgaia/logging-history/r-hglog/r-hglog/input/data/new/output/shps/"))
+
+# find proportion of intersected polygons that are within 5 years difference
+inter %<>% mutate(within5 = ifelse(abs(YearDiff) > 5, 0, 1),
+                  within10 = ifelse(abs(YearDiff) > 10, 0, 1)) 
+prop5 <- sum(inter$within5)/nrow(inter)
+prop10 <- sum(inter$within10)/nrow(inter)
 
 # plot difference in harvest year
 diffmap <- ggplot() + geom_sf(data = inter.grp, aes(fill = AveYearDif), color = "transparent") +
